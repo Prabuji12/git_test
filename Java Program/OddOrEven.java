479e8c654d3874aa139ec9e0e18fd581c9cf891b
SethuBlitzer <ping4sethu@gmail.com>
2022-06-16 19:35:12 +0530

    Added comments on a line in OddOrEven Program

--- Java Program/OddOrEven.java
+++ Java Program/OddOrEven.java
@@ -2,7 +2,7 @@ import java.util;
 class OddOrEven{
  public static void main(String[] arg){
   Scanner sc = new Scanner(System.in);
-  int input = sc.nextInt();
+  int input = sc.nextInt(); // To get the input number
   if(input%2==0){
    System.out.println("Given number is Even");
   }
